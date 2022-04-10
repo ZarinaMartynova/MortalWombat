71717c22b1d46e021348166e7e35372acd076725
Zarina Martynova <martynovazarina1985@gmail.com>
2022-04-10 12:42:40 +0300

    failing_test

--- src/main/java/net/absoft/mortalwombat/AgeChecker.java
+++ src/main/java/net/absoft/mortalwombat/AgeChecker.java
@@ -1,7 +1,7 @@
 package net.absoft.mortalwombat;
 
 public class AgeChecker {
-    private final int min_age =18;
+    private final int min_age =21;
     public boolean canPlayGame(int age){
         return age >= min_age;
     }
